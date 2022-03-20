1a194ee62523d51671d0e72f6c34e99279c4c803
black-code1 <legrandmahamat1@gmail.com>
2022-03-20 16:57:43 +0100

    Template Refs in the Setup Function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 // @ is an alias to /src
 
 export default {
   name: "HomeView",
   setup() {
-    console.log("setup");
+    console.log(this);
+
+    let p = ref("hello");
+    console.log(p, p.value);
 
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked ne");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "hello, coders";
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };
