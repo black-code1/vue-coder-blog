835aeeda196f1d6a0eaf5a811edfcdcf7092a90c
black-code1 <legrandmahamat1@gmail.com>
2022-03-21 09:33:17 +0100

    Using useRouter

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -15,12 +15,19 @@
 
 <script>
 import { ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+
 export default {
   setup() {
     const title = ref("");
     const body = ref("");
     const tag = ref("");
     const tags = ref([]);
+
+    const router = useRouter();
+    console.log(router);
+    // router.go(-1);
+    // router.go(1);
 
     const handleKeydown = () => {
       if (!tags.value.includes(tag.value)) {
@@ -42,6 +49,8 @@ export default {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(post),
       });
+
+      router.push({ name: "home" });
     };
     return { title, body, tag, handleKeydown, tags, handleSubmit };
   },
